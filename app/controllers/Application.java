36b2069bf415b0ee3b76729328b26013a808ca05
Evan Mallory <schmave@gmail.com>
2013-04-09 10:00:01 -0400

    Added ability to associate people with families.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,30 @@
 package controllers;
 
-import play.*;
-import play.data.*;
-import play.mvc.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import models.*;
 
+import play.*;
+import play.data.*;
+import play.libs.Json;
+import play.mvc.*;
+
 import views.html.*;
 
+/*
+
+    TODO
+
+* be able to edit a Person
+* tags
+* comments
+* phone numbers
+
+
+ */
 
 public class Application extends Controller {
 
@@ -22,21 +39,36 @@ public class Application extends Controller {
     }
 
     public static Result person(Integer id) {
-        return ok(views.html.person.render(Person.find.ref(id)));
+        Person the_person = Person.find.ref(id);
+        return ok(views.html.person.render(
+            the_person,
+            Person.find.where().isNotNull("family").eq("family", the_person.family).ne("person_id", the_person.person_id).findList()));
+    }
+
+    static String makeAutocompleteList(List<Person> people) {
+        List<Map<String, String> > result = new ArrayList<Map<String, String> > ();
+        for (Person p : people) {
+            HashMap<String, String> values = new HashMap<String, String>();
+            values.put("label", p.first_name + " " + p.last_name);
+            values.put("id", "" + p.person_id);
+            result.add(values);
+        }
+
+        return Json.stringify(Json.toJson(result));
     }
 
     public static Result newPerson() {
-        return ok(views.html.new_person.render(personForm));
+        return ok(views.html.new_person.render(personForm, makeAutocompleteList(Person.all())));
     }
 
     public static Result makeNewPerson() {
         Form<Person> filledForm = personForm.bindFromRequest();
         if(filledForm.hasErrors()) {
             return badRequest(
-                views.html.new_person.render(filledForm)
+                views.html.new_person.render(filledForm, makeAutocompleteList(Person.all()))
             );
         } else {
-            Person.create(filledForm.get());
+            Person.create(filledForm.get(), Integer.parseInt(filledForm.field("same_family_id").value()));
             return redirect(routes.Application.people());
         }
     }
--- app/models/Person.java
+++ app/models/Person.java
@@ -48,10 +48,28 @@ public class Person extends Model {
     );
 
     public static List<Person> all() {
-        return find.all();
+        return find.where().eq("is_family", Boolean.FALSE).findList();
     }
 
-    public static void create(Person person) {
+    public static void create(Person person, Integer same_family_id) {
+        person.is_family = false;
+
+        if (same_family_id != null) {
+            Person other_family_member = Person.find.ref(same_family_id);
+
+            if (other_family_member.family != null) {
+                // Attach to their existing family.
+                person.family = other_family_member.family;
+            } else {
+                // Create a new family to hold these two people.
+                Person family = new Person();
+                family.is_family = true;
+                family.save();
+                person.family = family;
+                other_family_member.family = family;
+                other_family_member.save();
+            }
+        }
         person.save();
     }
 
