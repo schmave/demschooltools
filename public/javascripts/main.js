b5b92d009be6fe7653e02505beed760b1ffd4798
Evan Mallory <schmave@gmail.com>
2013-04-11 08:34:59 -0400

    Added people search box to the navbar

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -7,5 +7,14 @@ $(document).ready(function () {
     $( "#same_family_name" ).bind( "autocompleteselect", function(event, ui) {
         $("#same_family_id").val(ui.item.id);
     });
+
+    $( "#navbar_people_search" ).autocomplete({
+        source: "/jsonPeople",
+        minLength: 2
+    });
+
+    $( "#navbar_people_search" ).bind( "autocompleteselect", function(event, ui) {
+        window.location.href="/people/" + ui.item.id;
+    });
 });
 
