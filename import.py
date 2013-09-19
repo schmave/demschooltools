0431b28fc0852fd3204d26a43d34e49bca1af50b
Evan Mallory <schmave@gmail.com>
2013-09-19 19:25:28 -0400

    Support empty names and empty addresses

--- import.py
+++ import.py
@@ -19,10 +19,13 @@ for line in csv.DictReader(open(sys.argv[1], 'rb')):
 		date = datetime.date(2013, 5, 19)
 	
 	name = line["Name"].strip()
+	if not name:
+		name = line["Email"]
 	name_splits = name.split(' ')
 	
-	ors = ([cur.mogrify("first_name like %s", [n]) for n in name_splits] + 
-		[cur.mogrify("last_name like %s",[n]) for n in name_splits])
+	ors = ([cur.mogrify("first_name ilike %s", [n]) for n in name_splits] + 
+		[cur.mogrify("last_name ilike %s",[n]) for n in name_splits] + 
+		[cur.mogrify("email ilike %s",[line["Email"]])])
 	ors = " OR ".join(ors)
 	
 	print "Known people like '" + name + "': "
@@ -38,13 +41,22 @@ for line in csv.DictReader(open(sys.argv[1], 'rb')):
 		cur.execute("INSERT into person (first_name, last_name) VALUES (%s, %s) returning person_id", (
 			" ".join(name_splits[:-1]), name_splits[-1]))
 		id = int(cur.fetchone()[0])
+	else:
+		cur.execute("SELECT description from donation where person_id=%s and description like 'Indiegogo --%%'", [id])
+		all_results = list(cur)
+		if all_results:
+			print "ID %d already has an indiegogo donation\n\n" % id
+			continue
 	print "ID: %d" % id
 	
-	cur.execute("UPDATE person set email='%s', address='%s', city='%s', state='%s', zip='%s' WHERE person_id=%d" % (
-		line["Email"], line["Address 1"], line["City"], line["State"], line["Zip"], id))
+	cur.execute("UPDATE person set email=%s where person_id=%s", (line["Email"], id))
+	if line["Address 1"]:
+		cur.execute("UPDATE person set address='%s', city='%s', state='%s', zip='%s' WHERE person_id=%d" % (
+			line["Address 1"], line["City"], line["State"], line["Zip"], id))
+
 	cur.execute("INSERT into donation (dollar_value, is_cash, description, person_id, date, thanked, indiegogo_reward_given) VALUES (%s, TRUE, '%s', %d, '%s', TRUE, TRUE)" % (
 		line["Amount"], "Indiegogo -- " + line["Perk"], id, date))
 	conn.commit()
-
+	
 cur.close()
 conn.close()
