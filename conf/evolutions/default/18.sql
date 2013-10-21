b1329dd956de0c2f06630f5dad67990cdd3ad491
Evan Mallory <schmave@gmail.com>
2013-10-20 20:05:46 -0400

    Incorporated db evolutions from schoolCRM

--- conf/evolutions/default/18.sql
+++ conf/evolutions/default/18.sql
@@ -1,29 +1,8 @@
 # --- !Ups
 
-ALTER TABLE person_at_meeting ADD COLUMN id serial;
-
-ALTER TABLE "case" ALTER meeting_id SET NOT NULL;
-ALTER TABLE "case" ADD COLUMN location VARCHAR(255) NOT NULL DEFAULT '';
-ALTER TABLE "case" ADD COLUMN writer_id INTEGER;
-ALTER TABLE "case" ADD constraint fk_case_writer foreign key (writer_id) references person(person_id);
-
-UPDATE "case" SET findings='' WHERE findings IS NULL;
-ALTER TABLE "case" ALTER findings SET NOT NULL;
-
-ALTER TABLE "testify_record" ADD COLUMN id serial;
-
-UPDATE charge SET resolution_plan='' WHERE resolution_plan IS NULL;
-ALTER TABLE charge ALTER resolution_plan SET NOT NULL;
+ALTER TABLE "person" ADD COLUMN grade varchar(8) not null default '';
 
 # --- !Downs
 
-ALTER TABLE person_at_meeting DROP COLUMN id;
+ALTER TABLE "person" DROP COLUMN grade;
 
-ALTER TABLE "case" ALTER meeting_id DROP NOT NULL;
-ALTER TABLE "case" DROP COLUMN location;
-ALTER TABLE "case" DROP COLUMN writer_id;
-ALTER TABLE "case" ALTER findings DROP NOT NULL;
-
-ALTER TABLE "testify_record" DROP COLUMN id;
-
-ALTER TABLE charge ALTER resolution_plan DROP NOT NULL;
