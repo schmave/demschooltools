5c7a1daf184eb56fc1dffc4a58c35f3ba0b336db
Evan Mallory <schmave@gmail.com>
2014-02-20 20:27:15 -0500

    Show who you are logged in as; timeout logins after 30 mins

--- app/controllers/Secured.java
+++ app/controllers/Secured.java
@@ -9,6 +9,7 @@ import models.User;
 
 import play.Logger;
 import play.mvc.Http.Context;
+import play.mvc.Http.Session;
 import play.mvc.Result;
 import play.mvc.Security;
 
@@ -19,22 +20,33 @@ public class Secured extends Security.Authenticator {
         Logger.debug("Secured::getUsername " + ctx);
 		final AuthUser u = PlayAuthenticate.getUser(ctx.session());
 
-		if (u != null) {
+        if (u != null) {
             User the_user = User.findByAuthUserIdentity(u);
-            if (the_user == null) {
-                return null;
-            }
-
-            return u.getId();
+            if (the_user != null) {
+				// If a user is logged in already, check the session timeout.
+				// If there is no timeout, or we can't parse it, or it's too old,
+				// don't count the user as being logged in.
+				Session sess = ctx.session();
+				if (sess.get("timeout") != null) {
+					try
+					{
+						long timeout = Long.parseLong(sess.get("timeout"));
+						if (System.currentTimeMillis() - timeout < 1000 * 60 * 30) {
+							sess.put("timeout", "" + System.currentTimeMillis());
+							return u.getId();
+						}
+					}
+					catch (NumberFormatException e) {
+					}
+				}
+			}
         }
-
-        PlayAuthenticate.storeOriginalUrl(ctx);
-
         return null;
 	}
 
 	@Override
 	public Result onUnauthorized(final Context ctx) {
+        PlayAuthenticate.storeOriginalUrl(ctx);
 		return redirect(controllers.routes.Public.index());
 	}
 }
