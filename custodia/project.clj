200e3998b92df727fbc6b23f321f5ae28c6f74e0
Steve Shogren <steve.a.shogren@gmail.com>
2014-11-22 08:05:54 -0500

    Adding embedded nrepl support

--- custodia/project.clj
+++ custodia/project.clj
@@ -6,9 +6,13 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.8"]
                  [ring/ring-jetty-adapter "1.2.2"]
+                 [org.clojure/tools.nrepl "0.2.5"]
                  [environ "0.5.0"]]
+
   :min-lein-version "2.0.0"
-  :plugins [[environ/environ.lein "0.2.1"]]
+  :main clojure-getting-started.web/-main
+  :plugins [[cider/cider-nrepl "0.8.1"]
+            [environ/environ.lein "0.2.1"]]
   :hooks [environ.leiningen.hooks]
   :uberjar-name "clojure-getting-started-standalone.jar"
   :profiles {:production {:env {:production true}}})
--- custodia/src/clojure_getting_started/web.clj
+++ custodia/src/clojure_getting_started/web.clj
@@ -3,13 +3,15 @@
             [compojure.handler :refer [site]]
             [compojure.route :as route]
             [clojure.java.io :as io]
+            [clojure.tools.nrepl.server :as nrepl-server]
+            [cider.nrepl :refer (cider-nrepl-handler)]
             [ring.adapter.jetty :as jetty]
             [environ.core :refer [env]]))
 
 (defn splash []
   {:status 200
    :headers {"Content-Type" "text/plain"}
-   :body (pr-str ["Hello" :from 'Heroku])})
+   :body (pr-str ["Hello there" :from 'Heroku])})
 
 (defroutes app
   (GET "/" []
@@ -18,6 +20,7 @@
        (route/not-found (slurp (io/resource "404.html")))))
 
 (defn -main [& [port]]
+  (nrepl-server/start-server :port 7888 :handler cider-nrepl-handler)
   (let [port (Integer. (or port (env :port) 5000))]
     (jetty/run-jetty (site #'app) {:port port :join? false})))
 
