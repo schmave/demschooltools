c31308142c1127d29b82e4565fff2db985ef863e
Steve Shogren <steve.a.shogren@gmail.com>
2014-12-08 17:05:35 -0500

    Adding a basic main page and create student page

--- custodia/src/clojure_getting_started/web.clj
+++ custodia/src/clojure_getting_started/web.clj
@@ -6,6 +6,7 @@
             [clojure.tools.nrepl.server :as nrepl-server]
             [cider.nrepl :refer (cider-nrepl-handler)]
             [ring.adapter.jetty :as jetty]
+            [ring.util.response :as resp]
             [com.ashafa.clutch :as couch]
             [clojure-getting-started.db :as db]
             [clojure-getting-started.database :as data]
@@ -13,19 +14,25 @@
             [net.cgrand.enlive-html :as enlive]
             [hiccup.core :as html]
             [environ.core :refer [env]]))
-(defn t []
-  (html/html [:form {:method "POST" :action "/student/create"}
-              [:label {:name "name"}]
-              [:input {:type "text" :name "name"}]
-              [:input {:type "submit" :value "Create"}]
-              ]))
 
-(enlive/deftemplate main-template "index.html" []
-  [:p.lead] (enlive/content (t)))
+(def create-student-form 
+  (html/html [:form {:method "POST" :action "/student/create" :role "form"}
+              [:div {:class "form-group"}
+               [:label {:for "s-name" :name "name"} "Name"]
+               [:input {:class "form-control" :id "s-name" :type "text" :name "name"}]]
+              [:button {:class "btn btn-default" :type "submit" :value "Create"}]]))
+
+(defn main-form []
+  (html/html [:div
+              [:a {:href "/student/create"} "Create Student"]]))
+
+(enlive/deftemplate main-template "index.html" [form]
+  [:p.lead] (enlive/html-content form ))
 
 (defroutes app
-  (GET "/" [] (apply str (main-template)))
-  (POST "/student/create" req "success")
+  (GET "/" [] (apply str (main-template (main-form))))
+  (GET "/student/create" [] (apply str (main-template create-student-form)))
+  (POST "/student/create" req (resp/redirect "/"))
   (route/resources "/")
   (ANY "*" []
        (route/not-found (slurp (io/resource "404.html")))))
