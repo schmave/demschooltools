e6e3d5c001ca47dac2bc3d9da5bf3c320ea8bd2d
Evan Mallory <schmave@gmail.com>
2015-02-21 11:31:47 -0500

    Keep track of when people are added to or removed from tags

---
+++ app/models/PersonTagChange.java
@@ -0,0 +1,55 @@
+package models;
+
+import com.avaje.ebean.validation.NotNull;
+
+import java.util.*;
+
+import javax.persistence.*;
+
+import play.data.*;
+import play.data.validation.Constraints.*;
+import play.db.ebean.*;
+
+
+@Entity
+public class PersonTagChange extends Model {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "person_tag_change_id_seq")
+    public int id;
+
+    @NotNull
+    @ManyToOne()
+    @JoinColumn(name="tag_id")
+    public Tag tag;
+
+    @NotNull
+    @ManyToOne()
+    @JoinColumn(name="person_id")
+    public Person person;
+
+    @NotNull
+    @ManyToOne()
+    @JoinColumn(name="creator_id")
+    public User creator;
+
+    @Column(insertable = false, updatable = false)
+    public Date time;
+
+    @NotNull
+    public boolean was_add;
+
+    public static Finder<Integer, PersonTagChange> find = new Finder(
+        Integer.class, PersonTagChange.class
+    );
+
+    public static PersonTagChange create(Tag t, Person p, User u, boolean was_add) {
+        PersonTagChange result = new PersonTagChange();
+        result.tag = t;
+        result.person = p;
+        result.creator = u;
+        result.was_add = was_add;
+
+        result.save();
+        return result;
+    }
+}
