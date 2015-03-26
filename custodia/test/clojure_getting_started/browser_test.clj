6563b1ce02cf124ff3cf49d0f68cb9fbcf33e9b4
Steve Shogren <steve.a.shogren@gmail.com>
2015-03-26 17:35:58 -0400

    Getting the page to open and open the student page

--- custodia/test/clojure_getting_started/browser_test.clj
+++ custodia/test/clojure_getting_started/browser_test.clj
@@ -4,12 +4,23 @@
 
 (deftest ^:integration loggin-in
   ;;(set-driver! {:browser :firefox} "http://shining-overseer-test.herokuapp.com/login")
-  (set-driver! {:browser :chrome} "http://localhost:5000/login")
+  (set-driver! {:browser :firefox} "http://localhost:5000/login")
 
   ;; (click "a[href*='login']")
   (wait-until #(exists? "#username"))
   (input-text "#username" "admin")
   (input-text "#password" "changeme")
   (submit "#password")
-  (exists? "a[value*='Alan Brown']")
+  (wait-until #(exists? "a#student-7"))
+  (click "a#student-7")
+  (wait-until #(exists? "div#student-total-row"))
+  (testing "student page totals"
+    (is (= (value "div#student-total-row")
+           "Attended: 3"))
+    (is (= true
+           (.contains (value "#student-total-row")
+                      "Attended: 3")))
+    (is (= true
+           (.contains (value "#student-total-row")
+                      "Absent: 2"))))
   (quit))
