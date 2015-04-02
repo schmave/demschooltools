494832496e93f5ec3c15fa39366885367b22eca9
Steve Shogren <steve.a.shogren@gmail.com>
2015-04-02 08:36:44 -0400

    First working webdriver test

--- custodia/test/clojure_getting_started/browser_test.clj
+++ custodia/test/clojure_getting_started/browser_test.clj
@@ -1,21 +1,41 @@
 (ns clojure-getting-started.browser-test
   (:require [clojure.test :refer :all]
+            [clojure.java.shell :as sh]
             [clj-webdriver.taxi :refer :all]))
 
+(defn click-student [id]
+  (wait-until #(visible? (str "a#student-" id)))
+  (click (str "a#student-" id))
+  (wait-until #(visible? "#studenttotalrow")))
+
+(defn submit-missing []
+  (wait-until #(visible? "#submit-missing"))
+  (click "#submit-missing"))
+
+(defn clickw [q]
+  (wait-until #(visible? q))
+  (click q))
+
 (deftest ^:integration loggin-in
+  (sh/sh "make" "load-aliased-dump")
   ;;(set-driver! {:browser :firefox} "http://shining-overseer-test.herokuapp.com/login")
   (set-driver! {:browser :firefox} "http://localhost:5000/login")
 
-  ;; (click "a[href*='login']")
-  (wait-until #(exists? "#username"))
+  (wait-until #(visible? "#username"))
   (input-text "#username" "admin")
   (input-text "#password" "changeme")
   (submit "#password")
-  (wait-until #(exists? "a#student-7"))
-  (click "a#student-7")
-  (wait-until #(exists? "div#studenttotalrow"))
+  (click-student 7)
+
   (testing "student page totals"
-    (is (= (value "div#studenttotalrow")
-           (text "div#studenttotalrow")
-           "Attended: 3")))
+    (is (= (text "#studenttotalrow")
+           "Attended: 3 - Absent: 4 - Excused: 7 - Overrides: 0 - Short: 5")))
+
+  (wait-until #(visible? "#sign-in"))
+  (click "#sign-in")
+  ;; (submit-missing)
+  (click-student 7)
+  (testing "student page totals"
+    (is (= (text "#studenttotalrow")
+           "Attended: 3 - Absent: 4 - Excused: 7 - Overrides: 0 - Short: 6")))
   (quit))
