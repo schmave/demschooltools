56a4b7e9fe1d3fd065a7bdb77e985f73d1ae93f6
Steve Shogren <steve.a.shogren@gmail.com>
2015-04-02 18:21:32 -0400

    Tweaking the project to have an uberjar name

--- custodia/test/clojure_getting_started/browser_test.clj
+++ custodia/test/clojure_getting_started/browser_test.clj
@@ -16,17 +16,34 @@
   (wait-until #(visible? q))
   (click q))
 
+(defn login []
+  (wait-until #(visible? "#username"))
+  (input-text "#username" "admin")
+  (input-text "#password" "changeme")
+  (submit "#password"))
+
+(deftest ^:integration absent-column
+  (sh/sh "make" "load-aliased-dump")
+  ;;(set-driver! {:browser :firefox} "http://shining-overseer-test.herokuapp.com/login")
+  (set-driver! {:browser :firefox} "http://localhost:5000/login")
+  (login)
+  (click-student 7)
+
+  (wait-until #(visible? "#confirmed-absent"))
+  (click "#sign-in")
+  ;; (submit-missing)
+  (click-student 7)
+  (testing "student page totals"
+    (is (= (text "#studenttotalrow")
+           "Attended: 3 - Absent: 4 - Excused: 7 - Overrides: 0 - Short: 6")))
+  (quit))
+
 (deftest ^:integration loggin-in
   (sh/sh "make" "load-aliased-dump")
   ;;(set-driver! {:browser :firefox} "http://shining-overseer-test.herokuapp.com/login")
   (set-driver! {:browser :firefox} "http://localhost:5000/login")
-
-  (wait-until #(visible? "#username"))
-  (input-text "#username" "admin")
-  (input-text "#password" "changeme")
-  (submit "#password")
+  (login)
   (click-student 7)
-
   (testing "student page totals"
     (is (= (text "#studenttotalrow")
            "Attended: 3 - Absent: 4 - Excused: 7 - Overrides: 0 - Short: 5")))
