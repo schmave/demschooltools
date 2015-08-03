7adbf93c605a8f6e6d64dc2d8b5ce08ac3440bec
Keith O'Brien <obrienkeithd@gmail.com>
2015-08-03 09:08:50 -0400

    render a student table

--- custodia/src/js/studenttable.jsx
+++ custodia/src/js/studenttable.jsx
@@ -2,17 +2,37 @@ var React = require('react'),
     studentStore = require('./StudentStore');
 
 module.exports = React.createClass({
-    getInitialState: function(){
+    getInitialState: function () {
         return {students: []};
     },
-    componentDidMount: function(){
+    componentDidMount: function () {
         studentStore.addChangeListener(this._onChange);
         this.setState({students: studentStore.getStudents()});
     },
     render: function () {
-        return <div>{this.state.students}</div>;
+        var rows = this.state.students.map(function (student) {
+            return <tr>
+                <td>{student.absent_today ? student.name : ''}</td>
+                <td>{!student.in_today && !student.absent_today ? student.name : ''}</td>
+                <td>{student.in_today ? student.name : ''}</td>
+                <td>{student.absent_today ? student.name : ''}</td>
+            </tr>;
+        });
+        return <table className="table">
+            <thead>
+                <tr>
+                    <th>Not Coming In</th>
+                    <th>Not Yet In</th>
+                    <th>In</th>
+                    <th>Out</th>
+                </tr>
+            </thead>
+            <tbody>
+                {rows}
+            </tbody>
+        </table>;
     },
-    _onChange: function(){
+    _onChange: function () {
         this.setState({students: studentStore.getStudents()});
     }
 });
