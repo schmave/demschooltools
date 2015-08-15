9b93aaf4a9b26c78e062efcfb3bcfa448db18dd9
Keith O'Brien <obrienkeithd@gmail.com>
2015-08-14 22:50:19 -0400

    fix for link CSS and add a string startsWith polyfill

--- custodia/resources/public/js/polyfill.js
+++ custodia/resources/public/js/polyfill.js
@@ -20,3 +20,10 @@ if(!Array.prototype.find){
         return undefined;
     };
 }
+
+if (!String.prototype.startsWith) {
+  String.prototype.startsWith = function(searchString, position) {
+    position = position || 0;
+    return this.indexOf(searchString, position) === position;
+  };
+}
