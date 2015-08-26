27940f661a627e20d9ba9feb2ece45942116440b
Keith O'Brien <obrienkeithd@gmail.com>
2015-08-26 19:02:10 -0400

    example

--- custodia/src/js/student.jsx
+++ custodia/src/js/student.jsx
@@ -27,52 +27,56 @@ var exports = React.createClass({
     componentWillUnmount: function () {
         studentStore.removeChangeListener(this._onChange);
     },
-    getMissingSwipe: function(){
+    getMissingSwipe: function () {
         var d = new Date();
-        if(this.state.student.last_swipe_date) {
+        if (this.state.student.last_swipe_date) {
             d = new Date(this.state.student.last_swipe_date + "T10:00:00");
         }
-        var missingdirection = (this.state.student.last_swipe_type =="in")?"out":"in";
-        if(!this.state.student.in_today
-           // && this.state.student.last_swipe_type == "in"
-           && this.state.student.direction == "out"){
+        var missingdirection = (this.state.student.last_swipe_type == "in") ? "out" : "in";
+        if (!this.state.student.in_today
+                // && this.state.student.last_swipe_type == "in"
+            && this.state.student.direction == "out") {
             missingdirection = "in";
             d = new Date();
         }
-        d.setHours((missingdirection =="in")?8:15);
-        d.setMinutes( 0 );
+        d.setHours((missingdirection == "in") ? 8 : 15);
+        d.setMinutes(0);
 
         this.setState({missingswipe: d, missingdirection: missingdirection});
         this.refs.missingSwipeCollector.show();
     },
-    swipeWithMissing: function(missing){
+    swipeWithMissing: function (missing) {
         var student = this.state.student,
             missing = this.refs.missing_swiperef.state.value;
         actionCreator.swipeStudent(student, student.direction, missing);
     },
-    validateSignDirection: function(direction) {
+    validateSignDirection: function (direction) {
         this.state.student.direction = direction;
         var missing_in = ((this.state.student.last_swipe_type == "out"
-                           || (this.state.student.last_swipe_type == "in" && !this.state.student.in_today)
-                           || !this.state.student.last_swipe_type)
-                          && direction == "out"),
-        missing_out = (this.state.student.last_swipe_type == "in"
-                       && direction == "in");
+            || (this.state.student.last_swipe_type == "in" && !this.state.student.in_today)
+            || !this.state.student.last_swipe_type)
+            && direction == "out"),
+            missing_out = (this.state.student.last_swipe_type == "in"
+            && direction == "in");
 
-        if(missing_in || missing_out) {
+        if (missing_in || missing_out) {
             this.getMissingSwipe();
         } else {
             actionCreator.swipeStudent(this.state.student, direction);
         }
     },
 
-    signIn: function () {this.validateSignDirection('in');},
-    signOut: function () {this.validateSignDirection('out');},
+    signIn: function () {
+        this.validateSignDirection('in');
+    },
+    signOut: function () {
+        this.validateSignDirection('out');
+    },
 
     markAbsent: function () {
         actionCreator.markAbsent(this.state.student);
     },
-    studentInToday: function() {
+    studentInToday: function () {
         return this.state.student.last_swipe_date == this.state.student.today;
     },
     getActionButtons: function () {
@@ -96,12 +100,12 @@ var exports = React.createClass({
 
         return buttons;
     },
-    getDayStatus: function(day){
-        if(day.excused){
+    getDayStatus: function (day) {
+        if (day.excused) {
             return '(Excused)';
-        }else if(day.override){
+        } else if (day.override) {
             return '(Given Attendence)';
-        }else{
+        } else {
             return '';
         }
     },
@@ -112,7 +116,9 @@ var exports = React.createClass({
         }
         return this.state.student.days.map(function (day, i) {
             return <tr className={day.day === this.getActiveDay(this.state.student) ? "selected" : ""}>
-                <td><Link to="student" params={{studentId: this.state.studentId, day: day.day}}>{day.day} {this.getDayStatus(day)}</Link></td>
+                <td><Link to="student"
+                          params={{studentId: this.state.studentId, day: day.day}}>{day.day} {this.getDayStatus(day)}</Link>
+                </td>
             </tr>;
         }.bind(this))
     },
@@ -123,73 +129,86 @@ var exports = React.createClass({
         actionCreator.updateStudent(this.state.student._id, this.refs.name.getDOMNode().value);
         this.toggleEdit();
     },
-    getActiveDay: function(student){
-        if(this.context.router.getCurrentParams().day){
+    getActiveDay: function (student) {
+        if (this.context.router.getCurrentParams().day) {
             return this.context.router.getCurrentParams().day;
-        }else if(this.state.student){
+        } else if (this.state.student) {
             return student.days[0].day;
-        }else{
+        } else {
             return '';
         }
     },
-    toggleHours : function() {
+    toggleHours: function () {
         this.state.student.olderdate = !!!this.state.student.olderdate;
         actionCreator.toggleHours(this.state.student._id);
     },
-    showingStudentName: function() {
+    showingStudentName: function () {
         return <div className="col-sm-8">
                   <span onClick={this.toggleEdit}>
-                    <h1 className="pull-left" >{this.state.student.name}</h1>
+                    <h1 className="pull-left">{this.state.student.name}</h1>
                     <span className="fa fa-pencil edit-student"></span>
                   </span>
-                  <h2 className="badge badge-red">{(!this.studentInToday() && this.state.student.absent_today) ? 'Absent' : ''}</h2>
-               </div>;
+
+            <h2 className="badge badge-red">{(!this.studentInToday() && this.state.student.absent_today) ? 'Absent' : ''}</h2>
+        </div>;
     },
-    editingStudentName: function() {
+    editingStudentName: function () {
         return <div className="col-sm-8 row">
-                 <div className="col-sm-3">
-                   <input ref="name" className="form-control" id="studentName"
-                          defaultValue={this.state.student.name}/>
-                   <button onClick={this.saveChange} className="btn btn-success">
-                      <i className="fa fa-check icon-large">Save</i></button>
-                   <button onClick={this.toggleEdit} className="btn btn-danger">
-                      <i className="fa fa-times"></i></button>
-                 </div>
-                 <div>
-            <div><input type="radio" name="older" onChange={this.toggleHours} checked={!this.state.student.olderdate}/> 300 Minutes</div>
-            <div><input type="radio" name="older" onChange={this.toggleHours} checked={this.state.student.olderdate}/> 330 Minutes</div>
-                 </div>
-               </div>;
+            <div className="col-sm-3">
+                <input ref="name" className="form-control" id="studentName"
+                       defaultValue={this.state.student.name}/>
+                <button onClick={this.saveChange} className="btn btn-success">
+                    <i className="fa fa-check icon-large">Save</i></button>
+                <button onClick={this.toggleEdit} className="btn btn-danger">
+                    <i className="fa fa-times"></i></button>
+            </div>
+            <div>
+                <div><input type="radio" name="older" onChange={this.toggleHours}
+                            checked={!this.state.student.olderdate}/> 300 Minutes
+                </div>
+                <div><input type="radio" name="older" onChange={this.toggleHours}
+                            checked={this.state.student.olderdate}/> 330 Minutes
+                </div>
+            </div>
+        </div>;
     },
     render: function () {
         if (this.state.student) {
             var activeDate = this.getActiveDay(this.state.student);
             return <div className="row">
-                <Modal ref="missingSwipeCollector" title={"What time did you sign " + this.state.missingdirection + "?"}>
+                <Modal ref="missingSwipeCollector"
+                       title={"What time did you sign " + this.state.missingdirection + "?"}>
                     <form className="form-inline">
                         <div className="form-group">
-                <label htmlFor="missing">What time did you sign {this.state.missingdirection}?</label>
-                <DateTimePicker id="missing" defaultValue={this.state.missingswipe} ref="missing_swiperef" calendar={false}/>
+                            <label htmlFor="missing">What time did you sign {this.state.missingdirection}?</label>
+                            <div>MissingSwipe no work: {this.state.missingswipe}</div>
+                            <div>MissingSwipe work: {this.state.missingswipe? this.state.missingswipe.toDateString() : ''}</div>
+                            <DateTimePicker id="missing" defaultValue={this.state.missingswipe} ref="missing_swiperef"
+                                            calendar={false}/>
                         </div>
                         <div className="form-group" style={{marginLeft: '2em'}}>
-                <button className="btn btn-sm btn-primary" onClick={this.swipeWithMissing}>Sign {this.state.missingdirection} </button>
+                            <button className="btn btn-sm btn-primary" onClick={this.swipeWithMissing}>
+                                Sign {this.state.missingdirection} </button>
                         </div>
                     </form>
                 </Modal>
+
                 <div className="col-sm-1"></div>
                 <div className="col-sm-10">
                     <div className="panel panel-info">
                         <div className="panel-heading">
                             <div className="row">
-                {!this.state.editing ? this.showingStudentName() : this.editingStudentName()}
+                                {!this.state.editing ? this.showingStudentName() : this.editingStudentName()}
                                 <div className="col-sm-4">
                                     <div className="col-sm-6"><b>Attended:</b> {this.state.student.total_days}</div>
                                     <div className="col-sm-6"><b>Absent:</b> {this.state.student.total_abs}</div>
                                     <div className="col-sm-6"><b>Excused:</b> {this.state.student.total_excused}</div>
-                                    <div className="col-sm-6"><b>Gave Attendence:</b> {this.state.student.total_overrides}
+                                    <div className="col-sm-6"><b>Gave
+                                        Attendence:</b> {this.state.student.total_overrides}
                                     </div>
                                     <div className="col-sm-6"><b>Short:</b> {this.state.student.total_short}</div>
-                                    <div className="col-sm-6"><b>Required Minutes:</b> {this.state.student.olderdate ? 330 : 300}</div>
+                                    <div className="col-sm-6"><b>Required
+                                        Minutes:</b> {this.state.student.olderdate ? 330 : 300}</div>
                                 </div>
                             </div>
                         </div>
@@ -199,7 +218,7 @@ var exports = React.createClass({
                                     <div className="row">
                                         {this.getActionButtons()}
                                     </div>
-                                    <Heatmap days={this.state.student.days} />
+                                    <Heatmap days={this.state.student.days}/>
                                 </div>
                                 <div className="col-sm-2">
                                     <table className="table table-striped center">
@@ -215,7 +234,7 @@ var exports = React.createClass({
                                 </div>
                                 <div className="col-sm-2">
                                     {activeDate && this.state.student
-                                        ? <Swipes student={this.state.student} day={activeDate} />
+                                        ? <Swipes student={this.state.student} day={activeDate}/>
                                         : ''}
                                 </div>
                             </div>
@@ -232,7 +251,7 @@ var exports = React.createClass({
     },
     _onChange: function () {
 
-        if(this.refs.missingSwipeCollector){
+        if (this.refs.missingSwipeCollector) {
             this.refs.missingSwipeCollector.hide();
         }
         var s = studentStore.getStudent(this.state.studentId);
