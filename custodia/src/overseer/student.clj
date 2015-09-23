2d54d5d47b4cc053164599b00be328486be95f64
Jack <steve.a.shogren@gmail.com>
2015-09-23 07:38:15 -0400

    Fixing the broken collect missing swipe

--- custodia/src/overseer/student.clj
+++ custodia/src/overseer/student.clj
@@ -13,9 +13,7 @@
 (defn student-page-response [student-id]
   (resp/response {:student (first (att/get-student-with-att student-id))}))
 
-(defn show-archived? []
-  (let [{roles :roles} (friend/current-authentication)]
-    (contains? roles roles/admin)))
+(defn show-archived? [] true)
 
 (defn get-student-list []
   (att/get-student-list (show-archived?)))
@@ -38,11 +36,6 @@
        (friend/authorize #{roles/admin}
                          (data/rename id name))
        (student-page-response id))
-
-  (POST "/students/:id/togglearchived" [id :<< as-int]
-        (friend/authorize #{roles/admin}
-                          (do (data/toggle-student-archived id)
-                              (student-page-response id))))
 
   (POST "/students/:id/togglehours" [id :<< as-int]
         (friend/authorize #{roles/admin}
@@ -70,6 +63,7 @@
                           (student-page-response id)))
 
   (POST "/students/:id/swipe" [id :<< as-int direction  missing]
+        (trace/trace "Posted" [id direction missing])
         (friend/authorize #{roles/user}
                           (if (= direction "in")
                             (do (when missing (data/swipe-out id missing))
