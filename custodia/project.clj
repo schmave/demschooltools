832e24998854b7cab73cb30ea64bdf3d6a10460c
Jack <steve.a.shogren@gmail.com>
2015-11-19 07:22:15 -0500

    Getting cljs to build correctly, still not on page

--- custodia/project.clj
+++ custodia/project.clj
@@ -3,7 +3,7 @@
   :url "http://overseer.herokuapp.com"
   :license {:name "Eclipse Public License v1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
                  [ring/ring-jetty-adapter "1.2.2"]
                  [org.clojure/tools.nrepl "0.2.7"]
@@ -22,11 +22,34 @@
                  [migratus "0.8.4"]
                  [org.postgresql/postgresql "9.4-1201-jdbc41"]
                  [org.clojure/tools.trace "0.7.8"]
-                 [com.ashafa/clutch "0.4.0"]]
+                 [com.ashafa/clutch "0.4.0"]
+
+                 ;; cljs
+                 [org.clojure/clojurescript "1.7.170" :scope "provided"]
+
+                 [reagent "0.5.1"]
+                 [reagent-forms "0.5.13"]
+                 [reagent-utils "0.1.5"]
+                 [secretary "1.2.3"]
+                 [org.clojure/core.async "0.2.374"]
+                 [cljs-ajax "0.5.1"]
+                 [markdown-clj "0.9.80"]
+                 [org.immutant/web "2.1.1" :exclusions [ch.qos.logback/logback-classic]]
+                 ]
   :min-lein-version "2.0.0"
   :plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
             [lein-ring "0.7.0"]
+            [lein-cljsbuild "1.1.1"]
             [environ/environ.lein "0.2.1"]]
+  :resource-paths ["resources" "target/cljsbuild"]
+  :cljsbuild
+  {:builds {:app {:source-paths ["src-cljs"]
+                  :compiler {:output-to     "target/cljsbuild/public/js/app.js"
+                             :output-dir    "target/cljsbuild/public/js/out"
+                             :source-map    true
+                             :externs       ["react/externs/react.js"]
+                             :optimizations :none
+                             :pretty-print  true}}}}
   :test-selectors {:default (or (complement :integration)
                                 (complement :performance))
                    :integration :integration
@@ -35,6 +58,13 @@
   ;; :main overseer.web
   :uberjar-name "overseer-standalone.jar"
   :profiles {:test {:dependencies [[clj-webdriver "0.6.1"]]}
-  	     :debug { :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"]}
+         :debug { :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"]}
              :production {:env {:production true}}
-             :uberjar {:main overseer.web :aot :all}})
+             :uberjar {:main overseer.web :aot :all
+                       :hooks ['leiningen.cljsbuild]
+                       :cljsbuild {:jar true
+                                   :builds {:app
+                                            {:compiler
+                                             {:optimizations :advanced
+                                              :pretty-print false}}}}
+                       }})
