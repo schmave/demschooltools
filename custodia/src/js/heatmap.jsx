b96d2b9e228e8cacc226ac313a3173009fea5033
Jack <steve.a.shogren@gmail.com>
2016-05-03 07:00:34 -0400

    Working to get the heatmap showing all days again

--- custodia/src/js/heatmap.jsx
+++ custodia/src/js/heatmap.jsx
@@ -2,6 +2,16 @@ var React = require('react'),
   heatmap = require('cal-heatmap'),
   Heatmapmonth = require('./heatmapmonth.jsx');
 
+Array.prototype.concatAll = function() {
+  var results = [];
+  this.forEach(function(subArray) {
+    subArray.forEach(function(subArrayValue) {
+      results.push(subArrayValue);
+    });
+  });
+  return results;
+};
+
 var groupingFunc = function (data) {
     return data.day.split('-')[0] + '-' + data.day.split('-')[1] + '-' + '01';
 };
@@ -12,12 +22,24 @@ module.exports = React.createClass({
         var minutes = this.props.requiredMinutes;
         var sortedDates = Object.keys(groupedDays).sort();
 
-        //var maps = sortedDates.map(function(date, idx){
-            return <Heatmapmonth index="1" 
+        var i,j,temparray,chunk = 4;
+        var maps =[];
+        for (i=0,j=sortedDates.length; i<j; i+=chunk) {
+            temparray = sortedDates.slice(i,i+chunk);
+            var dates = temparray.map(function(d) {
+                return groupedDays[d];
+            }).concatAll()
+            var m = this.makeHeatmapRow(dates, minutes);
+            maps.push(m);
+        };
+        //var maps = groupedDates.map(function(dates, idx){
+        //maps = maps.concatAll();
+        return maps
+    },
+    makeHeatmapRow: function(dates, minutes) {
+            return <Heatmapmonth index="1"
                                  requiredMinutes={minutes}
-                                 days={this.props.days}></Heatmapmonth>;
-        //});
-        //return maps;
+                                 days={dates}></Heatmapmonth>;
     },
     render: function () {
         return <div className="row">{this.loadHeatmaps()}</div>;
