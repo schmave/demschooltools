106f9ca881344257eed5f12962f51c41283d18c0
Evan Mallory <schmave@gmail.com>
2017-11-19 10:48:51 -0500

    Remove email form and show navbar on mobile

--- custodia/src/js/nav.jsx
+++ custodia/src/js/nav.jsx
@@ -22,20 +22,12 @@ module.exports = React.createClass({
         this.setState({selectedSchool: userStore.getSelectedSchool()});
     },
     render: function () {
-        return <nav className="navbar navbar-fixed-top" role="navigation">
+        return <nav className="navbar" role="navigation">
             <div className="container">
                 <div className="navbar-header">
-                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse"
-                            data-target="#navbar"
-                            aria-expanded="false" aria-controls="navbar">
-                        <span className="sr-only">Toggle navigation</span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                    </button>
                     <Link to="students" id="home" className="navbar-brand">{this.state.selectedSchool?this.state.selectedSchool.name:""} Custodia</Link>
                 </div>
-                <div id="navbar" className="collapse navbar-collapse">
+                <div id="navbar">
                     <ul className="nav navbar-nav">
                         <AdminWrapper>
                             <li><Link id="totals-link" to="reports">Reports</Link></li>
