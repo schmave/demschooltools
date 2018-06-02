411a9a53527ac7c8172f34e7ab8bb77bcb51e62c
Evan Mallory <schmave@gmail.com>
2018-06-02 16:01:23 -0400

    Add script to clone production db for dev use

---
+++ get_prod_db.bat
@@ -0,0 +1,7 @@
+set PG_PATH=\Program Files\PostgreSQL\9.4\bin\
+
+rem "%PG_PATH%pg_dump" -p 5433 -U evan -f school_crm.dump -Fc school_crm
+
+rem "%PG_PATH%pg_restore" -d school_crm -U postgres -W -e school_crm.dump
+
+"%PG_PATH%psql" -W -c "INSERT INTO organization_hosts VALUES ('localhost:9000', 1);DELETE FROM overseer.users;" school_crm postgres
