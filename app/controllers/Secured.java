a6468b573796ed8263a1db75b818ea68ccbecb7e
Evan Mallory <schmave@gmail.com>
2018-06-30 08:16:03 -0400

    Remove session timeout

--- app/controllers/Secured.java
+++ app/controllers/Secured.java
@@ -122,23 +122,8 @@ public class Secured {
             if (u != null) {
                 User the_user = User.findByAuthUserIdentity(u);
                 if (the_user != null) {
-    				// If a user is logged in already, check the session timeout.
-    				// If there is no timeout, or we can't parse it, or it's too old,
-    				// don't count the user as being logged in.
-    				Session sess = ctx.session();
-    				if (sess.get("timeout") != null) {
-    					try
-    					{
-    						long timeout = Long.parseLong(sess.get("timeout"));
-    						if (System.currentTimeMillis() - timeout < 1000 * 60 * 30) {
-    							sess.put("timeout", "" + System.currentTimeMillis());
-    							return the_user.email;
-    						}
-    					}
-    					catch (NumberFormatException e) {
-    					}
-    				}
-    			}
+					return the_user.email;
+				}
             }
 
             // If we don't have a logged-in user, try going by IP address.
