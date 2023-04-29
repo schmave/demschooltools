48767ab847169b2ff7b5b8dae87a6d0a4b2e0efc
Evan Mallory <evanmall@andrew.cmu.edu>
2023-04-29 07:51:49 -0400

    Don't override any existing values in set_keys_blank.sh

--- set_keys_blank.sh
+++ set_keys_blank.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export APPLICATION_SECRET="1234567890"
-export GOOGLE_CLIENT_SECRET="GOOG"
-export FACEBOOK_CLIENT_SECRET="FACE"
-export SES_PASSWORD="SES"
-export CUSTODIA_PASSWORD="web"
-export ROLLBAR_TOKEN="${ROLLBAR_TOKEN:empty-rollbar-token}"
+export APPLICATION_SECRET="${APPLICATION_SECRET:-asdlkfjhasldkjfhqo28329384u29q384asldkjfahslkdfj891}"
+export GOOGLE_CLIENT_SECRET="${GOOGLE_CLIENT_SECRET:-google-blank}"
+export FACEBOOK_CLIENT_SECRET="${FACEBOOK_CLIENT_SECRET:-facebook-blank}"
+export SES_PASSWORD="${SES_PASSWORD:-ses-blank-password}"
+export CUSTODIA_PASSWORD="${CUSTODIA_PASSWORD:-web}"
+export ROLLBAR_TOKEN="${ROLLBAR_TOKEN:-empty-rollbar-token}"
