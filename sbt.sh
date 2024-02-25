df193753b7979fe3dc399d2d0cdb076ded3e82e3
Evan Mallory <schmave@gmail.com>
2024-02-25 07:31:58 -0500

    Improve sbt.sh wrapper script

--- sbt.sh
+++ sbt.sh
@@ -2,4 +2,8 @@
 set -ex
 
 . set_keys_blank.sh
-PATH="~/jdk-11.0.2.jdk/Contents/Home/bin/:$PATH" /usr/local/bin/sbt $@
+JDK_11_PATH="~/jdk-11.0.2.jdk/Contents/Home/bin/:$PATH"
+
+PATH=$JDK_11_PATH java --version | grep '11.0' || (echo "Java 11 not found! sbt will probably fail"; exit 1)
+
+PATH=$JDK_11_PATH $HOMEBREW_PREFIX/bin/sbt $@
