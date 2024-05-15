1f16cb4be7862a3e5a4ffd68d38ca214ee067705
Connor Tyrrell <connor@brewgene.com>
2024-05-15 17:14:25 -0400

    Better logic for ensuring local version of Java is used

--- sbt.sh
+++ sbt.sh
@@ -2,10 +2,10 @@
 set -ex
 
 . set_keys_blank.sh
-JDK_11_PATH="~/jdk-11.0.2.jdk/Contents/Home/bin/:$PATH"
+export JAVA_HOME=~/jdk-11.0.18.jdk/Contents/Home
+JDK_11_PATH="$JAVA_HOME/bin:$PATH"
 
 PATH=$JDK_11_PATH java --version | grep '11.0' || (echo "Java 11 not found! sbt will probably fail"; exit 1)
 
 HOMEBREW_PREFIX="${HOMEBREW_PREFIX:-/usr/local}"
-
-PATH=$JDK_11_PATH $HOMEBREW_PREFIX/bin/sbt $@
+$HOMEBREW_PREFIX/bin/sbt $@
