da44ddb2240b86b2d4367e266c8c4b4cd5920f63
Evan Mallory <schmave@gmail.com>
2025-03-29 14:52:27 -0400

    Show search_fields to admin users

--- django/demschooltools/settings.py
+++ django/demschooltools/settings.py
@@ -84,7 +84,7 @@ ROOT_URLCONF = "demschooltools.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "demschooltools" / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
