225c95d164b185e790286d5d9edd717ccabad46a
Evan Mallory <schmave@gmail.com>
2025-04-10 15:31:48 -0400

    Add nohup& back to run_django.sh

--- django/run_django.sh
+++ django/run_django.sh
@@ -23,4 +23,4 @@ export DJANGO_SETTINGS_MODULE="demschooltools.settings_prod"
 
 uv run manage.py migrate
 uv run manage.py collectstatic --noinput
-uv run --group prod gunicorn --threads 4 --pid $PID_FILE demschooltools.wsgi >> ../dst-django.log 2>&1
+nohup uv run --group prod gunicorn --threads 4 --pid $PID_FILE demschooltools.wsgi >> ../dst-django.log 2>&1 &
