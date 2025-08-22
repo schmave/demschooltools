2e60bebccb55adf43ef2b8874357f961df94c139
Evan Mallory <schmave@gmail.com>
2025-08-22 14:38:19 -0400

    Fix static files caching in local development

--- django/demschooltools/settings.py
+++ django/demschooltools/settings.py
@@ -194,6 +194,9 @@ STATIC_URL = "/django-static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STORAGES = {
+    "staticfiles": {"BACKEND": "demschooltools.storage.RandomVersionStaticFilesStorage"}
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
