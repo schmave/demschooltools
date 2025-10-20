a596943e03eaf54f18373ea02e3242836a5b2b7b
Evan Mallory <schmave@gmail.com>
2025-10-19 22:19:08 -0400

    Get a working prod serve config

--- custodia/vite.config.js
+++ custodia/vite.config.js
@@ -1,38 +1,44 @@
 import react from "@vitejs/plugin-react";
 import { dirname, resolve } from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
-import { visualizer } from "rollup-plugin-visualizer";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export default defineConfig({
-  plugins: [react(), visualizer()],
-  root: ".",
-  publicDir: false, // Don't copy public directory since Django handles static files
-  build: {
-    outDir: resolve(__dirname, "..", "django", "static", "js"),
-    emptyOutDir: false,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
+export default ({ command, mode }) =>
+  defineConfig({
+    plugins: [react(), visualizer()],
+    root: ".",
+    publicDir: false, // Django handles static files
+    base: command == "build" ? "/django-static/custodia/" : "",
+    build: {
+      outDir: resolve(__dirname, "..", "django", "static", "custodia"),
+      emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          app: resolve(__dirname, "index.html"),
+        },
+        output: {
+          assetFileNames: `[name].[ext]`,
+          entryFileNames: `[name].js`,
+        },
+      },
+      manifest: true,
+      sourcemap: true,
+    },
+    server: {
+      port: 8082,
+      host: "0.0.0.0",
+      cors: true,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+        "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
       },
     },
-    manifest: true,
-    sourcemap: true,
-  },
-  server: {
-    port: 8082,
-    host: "0.0.0.0",
-    cors: true,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
+    resolve: {
+      extensions: [".js", ".jsx"],
     },
-  },
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-});
+  });
