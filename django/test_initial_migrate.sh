885b841bfd96043da95dec41329c6c9fcc033b27
Evan Mallory <schmave@gmail.com>
2025-11-12 15:35:38 -0500

    Update test_initial_migrate

--- django/test_initial_migrate.sh
+++ django/test_initial_migrate.sh
@@ -7,15 +7,14 @@ CREATE DATABASE school_crm;
 --- DROP ROLE IF EXISTS custodia;
 EOF
 
-# ./manage.py migrate dst 0001
-# ./manage.py migrate dst 0002 --fake
+pg_restore -d school_crm -U postgres -O -e ~/Downloads/backup_2025_11_12/db.dump
+# ./manage.py migrate dst 0001 --fake
 # ./manage.py migrate
 # pg_dump -O --schema-only school_crm > orig_sql.sql
 
 # ./manage.py migrate dst 0001 --fake
 # ./manage.py migrate dst 0002
 # ./manage.py migrate dst 0003 --fake
-./manage.py migrate
-pg_dump -O --schema-only school_crm > django_migrate.sql
-
-uv run manage.py setup_initial_data
+# ./manage.py migrate
+# pg_dump -O --schema-only school_crm > django_migrate.sql
+# uv run manage.py setup_initial_data
